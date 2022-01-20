9365c34fa918d9751745c5e139956c083d34b1fe
AthulK99 <93863965+AthulK99@users.noreply.github.com>
2022-01-20 22:26:07 +0530

    Add files via upload

---
+++ CO5/CO5_2.py
@@ -0,0 +1,13 @@
+f1=open("firstfile.txt","r")
+for x in f1:
+    print(x)
+f1.seek(0,0)
+print()
+f2=open("odd.txt","w")
+ff=f1.readlines()
+with open('odd.txt','w') as f2:
+    for x in range(0,len(ff)):
+        if(x%2!=0):
+            print(ff[x])
+            f2.write(ff[x])
+        
